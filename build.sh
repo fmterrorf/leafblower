ac7abdb48875d42d1e34382574fad267cb68cd7f
Daven Casia <77761194+fmterrorf@users.noreply.github.com>
2021-11-22 22:12:30 -0600

    Tweak build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ set -o errexit
 # Initial setup
 mix deps.get --only prod
 MIX_ENV=prod mix compile
-
-# Compile assets
-mix phx.digest
+MIX_ENV=prod mix assets.deploy
 
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
