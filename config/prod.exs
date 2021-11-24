e79997ac8d25672ea5bee65c865d8999719ecd60
Daven Casia <77761194+fmterrorf@users.noreply.github.com>
2021-11-23 21:57:30 -0600

    Changing port

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :leafblower, LeafblowerWeb.Endpoint,
-  url: [host: "${APP_NAME}.gigalixirapp.com", port: 80],
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
