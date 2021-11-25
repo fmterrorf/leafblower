3e58f80cb81f0e540b5002866e2c4b3a99bcae9a
Daven Casia <77761194+fmterrorf@users.noreply.github.com>
2021-11-24 23:14:54 -0600

    Setup fly

--- config/runtime.exs
+++ config/runtime.exs
@@ -32,7 +32,13 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
+  # IMPORTANT: Get the app_name we're using
+  app_name =
+    System.get_env("FLY_APP_NAME") ||
+      raise "FLY_APP_NAME not available"
+
   config :leafblower, LeafblowerWeb.Endpoint,
+    url: [host: "#{app_name}.fly.dev", port: 80],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
